e99b75ca900b3246a8ab03a5fc5a123660e42e14
zb-coder32 <22zbegg@gmail.com>
2025-12-05 21:10:32 +0000

    Update movement.java
    This should make the robot drive forward and backwards

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement.java
@@ -1,5 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-public class movement {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp(name = "Tank Drive Forward Backward", group = "TeleOp")
+public class movement extends LinearOpMode {
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    @Override
+    public void runOpMode() {
+
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+
+        // Reverse one motor so both go forward correctly
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double drive = -gamepad1.left_stick_y;
+
+            leftMotor.setPower(drive);
+            rightMotor.setPower(drive);
+        }
+    }
 }
+
