6cbd218abed5be88f0702817e4ad69de559066d3
KRISHNA <raturiryan@gmail.com>
2025-12-05 12:05:19 +0000

    New class
    Created a new intake class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeOpMode.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+public class IntakeOpMode  {
+
+}
