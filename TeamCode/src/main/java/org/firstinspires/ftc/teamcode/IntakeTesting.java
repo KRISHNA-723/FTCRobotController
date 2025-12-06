c9587ef48a10f82fc013ba9fbab275e2cd4433c5
KRISHNA <raturiryan@gmail.com>
2025-12-06 17:28:31 +0000

    Update to Intake
    Added few comments and removed spaces

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTesting.java
@@ -1,15 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.IntakeMode;
 
 
-
+// Testing the intake in the main TeleOp
 public class IntakeTesting extends OpMode {
 
     // Instantiating the IntakeMode class inside the OpMode class
     IntakeMode intake = new IntakeMode();
+
     @Override
     public void init() {
         intake.init(hardwareMap); // Get the hardware map for the intake
