4185b47173283b00754f47051d2ca7d7f2acb36d
KRISHNA <raturiryan@gmail.com>
2025-12-06 17:25:59 +0000

    Created Intake testing class
    Add IntakeMode class and IntakeTesting OpMode
    
    - IntakeMode handles intake motor functions
    - IntakeTesting OpMode tests the intake during TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTesting.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.teamcode.IntakeMode;
+
+
+
+public class IntakeTesting extends OpMode {
+
+    // Instantiating the IntakeMode class inside the OpMode class
+    IntakeMode intake = new IntakeMode();
+    @Override
+    public void init() {
+        intake.init(hardwareMap); // Get the hardware map for the intake
+    }
+
+
+    @Override
+    public void loop() {
+        boolean leftTrig = gamepad1.left_bumper; // Read left bumper input
+
+        if (leftTrig) {
+            intake.setMotorSpeed(0.5); // Run intake at 50% power
+        } else {
+            intake.setMotorSpeed(0); // Stop intake
+        }
+
+    }
+
+}
